5d4ec834a113e6416a0b638a03b9ef2b007a0f5e
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-08 19:16:27 +0530

    Added equality of line

--- LineCompar.java
+++ LineCompar.java
@@ -3,8 +3,8 @@ public class LineCompar
 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-		int x1, x2, y1, y2;
-		double lengthOfLine;
+		int x1, x2, y1, y2, x3, x4, y3, y4;
+		double lengthOfLine1, lengthOfLine2;
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter the point x1: ");
 		x1 = in.nextInt();
@@ -14,7 +14,26 @@ public class LineCompar
 		y1 = in.nextInt();
 		System.out.println("enter the point y2: ");
 		y2 = in.nextInt();
-		lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Length of the line: " + lengthOfLine);
-	}
+		System.out.println("enter the point x3: ");
+		x3 = in.nextInt();
+		System.out.println("enter the point x4: ");
+		x4 = in.nextInt();
+		System.out.println("enter the point y3: ");
+		y3 = in.nextInt();
+		System.out.println("enter the point y4: ");
+		y4 = in.nextInt();
+		lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		lengthOfLine2 = Math.sqrt((x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4));
+		System.out.println("Length of the line1: " + lengthOfLine1);
+		System.out.println("Length of the line2: " + lengthOfLine2);
+		if (lengthOfLine1 == lengthOfLine2) 
+		{
+            System.out.println("Both the lines are equal");
+        }
+        else 
+		{
+            System.out.println("Both the lines are not equal");
+        }
+		}
+}}
 }
